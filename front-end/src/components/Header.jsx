02358a8bc6d08662171517e46bf59f8a1fea4d49
marcioHUNK <marciohenrique12@hotmail.com>
2025-05-13 19:34:52 -0300

    correção do nome e do logo - feito em casa

--- front-end/src/components/Header.jsx
+++ front-end/src/components/Header.jsx
@@ -1,16 +1,16 @@
 import React from "react";
-import logoSpotify from "../assets/logo/spotify-logo.png";
+import logoSpotikunay from "../assets/logo/spotikunay-logo.png";
 import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <div className="header">
       <Link to="/">
-        <img src={logoSpotify} alt="Logo do Spotify" />
+        <img src={logoSpotikunay} alt="Logo do Spotify" />
       </Link>
 
       <Link to="/" className="header__link">
-        <h1>Spotify</h1>
+        <h1>Spotikunay</h1>
       </Link>
     </div>
   );
